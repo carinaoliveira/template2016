8c9c9047b98b963b53edb669f7e446b279b3e098
Carina Oliveira <contato@carinasoliveira.com>
2016-01-20 22:12:29 -0200

    TemplatePorfolio_Carina

---
+++ content/content-portfolioalt.php
@@ -0,0 +1,46 @@
+<div class="row">
+      
+	<div class="span8">
+	  <?php
+	  	/**
+	  	 * ebor_post_format hook
+	  	 * All actions contained in /ebor_framework/theme_actions.php
+	  	 *
+	  	 * @hooked ebor_post_format_markup - 10
+	  	 */
+	  	do_action('ebor_post_format');
+	  ?>
+	</div>
+	
+	<div class="span4 lp20">
+	
+	  <h2 class="post-title"><?php the_title(); ?></h2>
+	  
+	  <div class="divide5"></div>
+	  	<?php 
+	  		the_content(); 
+	  		wp_link_pages();
+	  	?>
+	  <div class="divide5"></div>
+	  
+	  <?php 
+	  	/**
+	  	 * ebor_portfolio_meta hook
+	  	 * All actions contained in /ebor_framework/theme_actions.php
+	  	 *
+	  	 * @hooked ebor_portfolio_meta_markup - 10
+	  	 */
+	  	do_action('ebor_portfolio_meta'); 
+	  	
+	  	/**
+	  	 * ebor_social_sharing hook
+	  	 * All actions contained in /ebor_framework/theme_actions.php
+	  	 *
+	  	 * @hooked ebor_social_sharing_markup - 10
+	  	 */
+	  	do_action('ebor_social_sharing'); 
+	  ?>
+	  
+	</div>
+	
+</div>
